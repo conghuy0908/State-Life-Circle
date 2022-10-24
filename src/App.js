f4f58217d078d4a0dee9d87eb1a44f0cec9a0b93
conghuy0908 <conghuy0908@gmail.com>
2022-10-24 16:08:31 +0700

    Ex1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
+import React, { Component } from 'react';
+ 
+class App extends Component {
+  constructor (props) {
+    super(props);
+    this.state = {
+      isExpand: false
+    };
+  }
+  handleShow = () =>{
+    this.setState({isExpand:true})
+  };
+  handleHidden = () =>{
+    this.setState({isExpand:false})
+  };
+  render() {
+    const {isExpand} = this.state;
+    if (isExpand) {
+      return(
+        <div>
+          <button onClick={this.handleHidden}>Đóng giới thiệu</button>
+          <h1>Introduction</h1>
+          <p>Conditional Rendering</p>
+        </div>
+      )
+    } else {
+      return(
+        <div>
+        <button onClick={this.handleShow} >Xem giới thiệu</button>
+        </div>
+      )
+    }
+    
+  }
+};
 export default App;
